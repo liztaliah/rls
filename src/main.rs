7c1e19ba39d1ccfc97f0902edac495973c556510
liztaliah <liz.t.hardee@gmail.com>
2024-10-18 06:51:10 -0700

    added filepath functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use std::fs;
+use std::{env, fs};
 
 fn main() {
-    match fs::read_dir(".") {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        list_dir(".");
+    }
+    else {
+        list_dir(args[1].as_str());
+    }
+}
+
+fn list_dir(direc: &str) {
+    match fs::read_dir(direc) {
         Ok(entries) => {
             for entry in entries {
                 match entry {
